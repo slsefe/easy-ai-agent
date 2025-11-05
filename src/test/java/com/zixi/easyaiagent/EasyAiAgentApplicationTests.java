f52672fbd3f8ba0897889e8c729dbfea59877ef8
baiyin <17693374267@163.com>
2025-11-05 20:15:55 +0800

    初始化

---
+++ src/test/java/com/zixi/easyaiagent/EasyAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zixi.easyaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
