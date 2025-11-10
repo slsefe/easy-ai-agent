528065c52fa20d46f3a5a60bac5652b97148c8f8
baiyin <17693374267@163.com>
2025-11-10 17:44:28 +0800

    RAG：实现TikaDocumentReader

---
+++ src/main/java/com/zixi/easyaiagent/rag/MyTikaDocumentReader.java
@@ -0,0 +1,27 @@
+package com.zixi.easyaiagent.rag;
+
+import org.springframework.ai.document.Document;
+import org.springframework.ai.reader.tika.TikaDocumentReader;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * 使用Apache Tika从多种文档格式中提取文档，比如PDF、DOC/DOCX、PPT/PPTX、HTML
+ * <a href="https://tika.apache.org/3.1.0/formats.html">Apache Tika</a>
+ */
+@Component
+public class MyTikaDocumentReader {
+
+    private final Resource resource;
+
+    public MyTikaDocumentReader(@Value("classpath:/word-sample.docx") Resource resource) {
+        this.resource = resource;
+    }
+
+    public List<Document> loadText() {
+        return new TikaDocumentReader(resource).read();
+    }
+}
