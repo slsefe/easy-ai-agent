b799347729cd1465f83d2e08c331b51caa9bd350
baiyin <17693374267@163.com>
2025-11-10 21:45:38 +0800

    RAG：向量化存储时增加关键词元数据

--- src/main/java/com/zixi/easyaiagent/config/LoveAppVectorStoreConfig.java
+++ src/main/java/com/zixi/easyaiagent/config/LoveAppVectorStoreConfig.java
@@ -1,6 +1,7 @@
 package com.zixi.easyaiagent.config;
 
 import com.zixi.easyaiagent.rag.reader.LoveAppDocumentLoader;
+import com.zixi.easyaiagent.rag.transformer.DocumentMetadataEnricher;
 import jakarta.annotation.Resource;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.embedding.EmbeddingModel;
@@ -18,14 +19,19 @@ public class LoveAppVectorStoreConfig {
     @Resource
     private LoveAppDocumentLoader loveAppDocumentLoader;
 
+    @Resource
+    private DocumentMetadataEnricher documentMetadataEnricher;
+
     @Bean
     VectorStore loveAppVectorStore(@Qualifier("dashscopeEmbeddingModel") EmbeddingModel embeddingModel) {
         // 创建向量化存储
         SimpleVectorStore simpleVectorStore = SimpleVectorStore.builder(embeddingModel).build();
         // 本地知识库加载文档
         List<Document> documents = loveAppDocumentLoader.loadMarkdownDocuments();
+        // 自动补充关键词元信息
+        List<Document> enrichedDocuments = documentMetadataEnricher.enrichDocuments(documents, 5);
         // 向量化存储
-        simpleVectorStore.add(documents);
+        simpleVectorStore.add(enrichedDocuments);
         return simpleVectorStore;
     }
 }
