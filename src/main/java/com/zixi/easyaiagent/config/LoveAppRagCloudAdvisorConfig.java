e9bd70c1f2aa92af1ed6cb5ed3ddfce077fb4c69
baiyin <17693374267@163.com>
2025-11-11 13:56:50 +0800

    RAG：增加注释

--- src/main/java/com/zixi/easyaiagent/config/LoveAppRagCloudAdvisorConfig.java
+++ src/main/java/com/zixi/easyaiagent/config/LoveAppRagCloudAdvisorConfig.java
@@ -10,6 +10,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * 自定义基于阿里云知识库服务的RAG增强Advisor
+ */
 @Configuration
 public class LoveAppRagCloudAdvisorConfig {
 
@@ -17,7 +20,7 @@ public class LoveAppRagCloudAdvisorConfig {
     private String dashScopeApiKey;
 
     /**
-     * 基于云知识库实现RAG
+     * 基于阿里云知识库实现RAG
      * @return RetrievalAugmentationAdvisor
      */
     @Bean
